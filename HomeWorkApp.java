5a469185a4d4ea29b26abcd09282ff6f3a65ae64
Artem Meshcherov <spacenail@gmail.com>
2021-05-17 22:51:06 +0300

    rename method sum->firstMethode second method

--- HomeWorkApp.java
+++ HomeWorkApp.java
@@ -2,11 +2,19 @@ public class HomeWorkApp {
     public static void main(String[] args) {
 
     }
-    static boolean sum(int a, int b){
+    static boolean firstMethod(int a, int b){
         if((a + b >= 10) && (a + b <= 20)) {
             return true;
         } else {
             return false;
+        }
+    }
+
+    static void secondMethod(int a){
+        if(a >= 0){
+            System.out.println("Число а=" + a +" положительное");
+        } else {
+            System.out.println("Число а=" + a +" отрицательное");
         }
     }
 }
