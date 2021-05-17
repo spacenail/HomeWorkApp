40e0ee5c8aeef27e6acd975dd65fd9300b7aaaaa
Artem Meshcherov <spacenail@gmail.com>
2021-05-17 22:45:10 +0300

    homework2 first method

--- HomeWorkApp.java
+++ HomeWorkApp.java
@@ -1,2 +1,12 @@
 public class HomeWorkApp {
+    public static void main(String[] args) {
+
+    }
+    static boolean sum(int a, int b){
+        if((a + b >= 10) && (a + b <= 20)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
