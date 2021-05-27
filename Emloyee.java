77e98cdc4355d8286d9cd2688526832410fe47ba
Artem Meshcherov <spacenail@gmail.com>
2021-05-27 22:39:48 +0300

    task 1-2

---
+++ Emloyee.java
@@ -0,0 +1,23 @@
+public class Emloyee {
+    String fullName;
+    String position;
+    String email;
+    String phoneNumber;
+    int salary;
+    int age;
+    
+    Emloyee (String fullName,
+            String position,
+            String email,
+            String phoneNumber,
+            int salary,
+            int age){
+
+        this.fullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.salary = salary;
+        this.age = age;
+    }
+}
