064a9cb5d34ff6b25941d7c5743e4f3e907c9b40
Artem Meshcherov <spacenail@gmail.com>
2021-06-01 13:18:53 +0300

    -fix string output

--- Cat.java
+++ Cat.java
@@ -3,19 +3,20 @@ public class Cat extends Animal{
 
     public Cat(String name){
         super(name);
+        System.out.println("Родился кот " + name);
     }
 
     @Override
     void run(int distance) {
         if(distance > maxRunDistance) {
-            System.out.printf("%s - очень старался, но пробежал только %dм \n", name, maxRunDistance);
+            System.out.printf("Кот %s - очень старался, но из %dм пробежал только %dм \n", name,distance, maxRunDistance);
         } else {
-            System.out.printf("%s - пробежал %dм \n", name, distance);
+            System.out.printf("Кот %s - пробежал %dм \n", name, distance);
         }
     }
 
     @Override
     void swim(int distance) {
-        System.out.printf("Кот %s не умеет плавать(как и все кошки)", name);
+        System.out.printf("Кот %s не умеет плавать(как и все кошки)\n", name);
     }
 }
--- Dog.java
+++ Dog.java
@@ -4,23 +4,24 @@ public class Dog extends Animal {
 
     public Dog(String name) {
         super(name);
+        System.out.println("Родился пёс " + name);;
     }
 
     @Override
     void run(int distance) {
         if (distance > maxRunDistance) {
-            System.out.printf("%s - очень старался, но пробежал только %dм \n", name, maxRunDistance);
+            System.out.printf("Пёс %s - очень старался, но из %dм пробежал только %dм \n", name, distance, maxRunDistance);
         } else {
-            System.out.printf("%s - пробежал %dм \n", name, distance);
+            System.out.printf("Пёс %s - пробежал %dм \n", name, distance);
         }
     }
 
     @Override
     void swim(int distance) {
         if (distance > maxSwimDistance) {
-            System.out.printf("%s - очень старался, но проплыл только %dм \n", name, maxRunDistance);
+            System.out.printf("Пёс %s - очень старался, но проплыл только %dм \n", name, maxRunDistance);
         } else {
-            System.out.printf("%s - проплыл %dм \n", name, distance);
+            System.out.printf("Пёс %s - проплыл %dм \n", name, distance);
         }
     }
 }
